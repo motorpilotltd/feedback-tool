9e6b8bf540d3c2c70e20163085228305f5c8638d
mplt-aleks <aleks@motorpilot.com>
2024-06-25 21:04:58 +0800

    Fixed LogLogin type hint

--- app/Http/Middleware/LogLogin.php
+++ app/Http/Middleware/LogLogin.php
@@ -5,14 +5,14 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Symfony\Component\HttpFoundation\Response;
+use Laravel\Fortify\Http\Responses\LoginResponse;
 
 class LogLogin
 {
     /**
      * Handle an incoming request.
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next): LoginResponse
     {
         Log::info('This user is logging in: '.$request->email);
 
