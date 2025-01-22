1987106d3963330841001d2970be5f036b441f94
Simon <333158+mplt-simon@users.noreply.github.com>
2025-01-22 11:29:13 +0000

    Laravel 11.39.0 Shift (#85)
    * Bump Laravel version constraint
    
    * composer update
    
    * Fixup Fortify migration issue.
    
    ---------
    
    Co-authored-by: Shift <shift@laravelshift.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,7 @@ class User extends Authenticatable
         'remember_token',
         'two_factor_recovery_codes',
         'two_factor_secret',
+        'two_factor_confirmed_at',
         'azure_token',
     ];
 
