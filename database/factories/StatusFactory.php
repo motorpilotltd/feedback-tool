899d58a7abe55e3005d8d2ac726966f4881051dc
Shift <shift@laravelshift.com>
2025-03-02 21:55:42 +0000

    Shift cleanup

--- database/factories/StatusFactory.php
+++ database/factories/StatusFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\Status;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class StatusFactory extends Factory
--- database/factories/VoteFactory.php
+++ database/factories/VoteFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\Vote;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class VoteFactory extends Factory
