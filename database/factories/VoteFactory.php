d862ec4c4b83907d51ac70e1e264d0f0cf613812
Shift <shift@laravelshift.com>
2025-03-02 21:55:31 +0000

    Remove unnecessary `$model` property

--- database/factories/VoteFactory.php
+++ database/factories/VoteFactory.php
@@ -8,13 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class VoteFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Vote::class;
-
-    /**
      * Define the model's default state.
      */
     public function definition(): array
