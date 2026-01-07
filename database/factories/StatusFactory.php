6a85d169b5d7159d0c6570673d28310ab1c5615f
Shift <shift@laravelshift.com>
2026-01-07 08:38:49 +0000

    Remove unnecessary `$model` property

--- database/factories/StatusFactory.php
+++ database/factories/StatusFactory.php
@@ -8,13 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class StatusFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Status::class;
-
-    /**
      * Define the model's default state.
      */
     public function definition(): array
