0aeaa235be1f3c789bb78db8fd3dada626b41431
Shift <shift@laravelshift.com>
2026-03-18 08:56:17 +0000

    Adopt new PHP attributes for simple properties

--- app/Models/CommentSpam.php
+++ app/Models/CommentSpam.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Table;
+use Illuminate\Database\Eloquent\Attributes\Unguarded;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[Table('comment_spam')]
+#[Unguarded]
 class CommentSpam extends Model
 {
     use HasFactory;
 
-    public $table = 'comment_spam';
-
-    protected $guarded = [];
 }
